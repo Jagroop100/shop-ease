3f284be0316ef65de49d14c3e6d857dabc2b24b8
Jagroop100 <jagsingh@stud.macromedia.de>
2026-06-08 22:24:49 +0200

    Initial Set up

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 
 
 app.get("/", (req, res) => {
-    res.send("Welcome to ShopEasy!");
+    res.send("Welcome to ShopEase!");
 });
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-const app = require(".app");
-const PORT = Process.env.PORT || 3000;
-const server = app.listen(PORT, ()=>{
-    console.log('Server is running on port $ {PORT}');
-})
-module.exports =server;
+const app = require("./app");
+const PORT = process.env.PORT || 3000;
+
+const server = app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+module.exports = server;
